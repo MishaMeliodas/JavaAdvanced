1686e3110354e32ef325d895a084cbc24e304d7b
Misha Meliodas <lytovchenko.mykhailo98@gmail.com>
2025-05-14 21:01:39 +0300

    Modified newClass.Java

--- src/lesson04/NewClass.java
+++ src/lesson04/NewClass.java
@@ -5,6 +5,6 @@ import java.util.Comparator;
 public class NewClass implements Comparator<Integer> {
     @Override
     public int compare(Integer o1, Integer o2) {
-        return o2 - o1;
+        return o1 - o2;
     }
 }
